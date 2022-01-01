69fccbf9604ed78b1b22bbaa1cb68a0a1d466600
shreyagoel01 <68478954+shreyagoel01@users.noreply.github.com>
2022-01-01 15:02:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react/cjs/react.development';
+import { useState } from 'react';
 import './App.css';
 import Alert from './components/Alert';
 //import About from './components/About';
